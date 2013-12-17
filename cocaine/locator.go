96999b94d3e41178934abad29e6a86c863eb59e7
noxiouz <noxiouz@yandex.ru>
2013-12-17 23:28:46 +0400

    Parse flags in NewWorker. It avoids cmd args clashing, when somebody uses cocaine for cli. Use --locator as default locator instead of wrong localhost:10053

--- cocaine/locator.go
+++ cocaine/locator.go
@@ -2,6 +2,7 @@ package cocaine
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"time"
@@ -44,7 +45,10 @@ type Locator struct {
 }
 
 func NewLocator(args ...interface{}) (*Locator, error) {
-	endpoint := "localhost:10053"
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+	endpoint := flag.flagLocator
 
 	if len(args) == 1 {
 		if _endpoint, ok := args[0].(string); ok {
--- cocaine/worker.go
+++ cocaine/worker.go
@@ -22,7 +22,6 @@ func init() {
 	flag.StringVar(&flagEndpoint, "endpoint", "", "Connection path")
 	flag.StringVar(&flagApp, "app", "standalone", "Connection path")
 	flag.StringVar(&flagLocator, "locator", "", "Connection path")
-	flag.Parse()
 }
 
 const (
@@ -151,6 +150,7 @@ type Worker struct {
 
 // Create new instance of Worker. Return error on fail.
 func NewWorker() (worker *Worker, err error) {
+	flag.Parse()
 	sock, err := newAsyncRWSocket("unix", flagEndpoint, time.Second*5)
 	if err != nil {
 		return
