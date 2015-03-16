55a17192c30953dff375ea6eea33ed78d589786e
Anton Tiurin <noxiouz@yandex.ru>
2015-03-16 07:58:58 +0300

    [Test] Add a test for hanlder

--- cocaine/worker/z_test.go
+++ cocaine/worker/z_test.go
@@ -27,11 +27,29 @@ func testConn() (io.ReadWriteCloser, io.ReadWriteCloser) {
 	return &pipeConn{read1, write2}, &pipeConn{read2, write1}
 }
 
+func checkTypeAndSession(t *testing.T, msg *Message, eSession uint64, eType uint64) {
+	if msg.MsgType != eType {
+		t.Fatalf("%d expected, but got %d", eType, msg.MsgType)
+	}
+	if msg.Session != eSession {
+		t.Fatal("Bad session number: %d instead of %d", msg.Session, eSession)
+	}
+}
+
 func TestWorker(t *testing.T) {
+	const (
+		testId      = "uuid"
+		testSession = 10
+	)
+
+	var (
+		onStop = make(chan struct{})
+	)
+
 	in, out := testConn()
 	sock, _ := NewAsyncRW(out)
 	sock2, _ := NewAsyncRW(in)
-	w, err := newWorker(sock, "test")
+	w, err := newWorker(sock, testId)
 	if err != nil {
 		t.Fatal("unable to create worker", err)
 	}
@@ -41,11 +59,40 @@ func TestWorker(t *testing.T) {
 		fmt.Println(data)
 		res.Write(data)
 		res.Close()
-		w.Stop()
 	})
+	go func() {
+		w.loop()
+		close(onStop)
+	}()
 
-	sock2.Write() <- NewInvoke(1, "test")
-	sock2.Write() <- NewChunk(1, "Dummy")
-	sock2.Write() <- NewChoke(1)
-	w.loop()
+	sock2.Write() <- NewInvoke(testSession, "test")
+	sock2.Write() <- NewChunk(testSession, "Dummy")
+	sock2.Write() <- NewChoke(testSession)
+
+	// handshake
+	eHandshake := <-sock2.Read()
+	checkTypeAndSession(t, eHandshake, 0, HandshakeType)
+
+	switch uuid := eHandshake.Payload[0].(type) {
+	case string:
+		if uuid != testId {
+			t.Fatal("bad uuid")
+		}
+	case []uint8:
+		if string(uuid) != testId {
+			t.Fatal("bad uuid")
+		}
+	default:
+		t.Fatal("No uuid")
+	}
+
+	eHeartbeat := <-sock2.Read()
+	checkTypeAndSession(t, eHeartbeat, 0, HeartbeatType)
+
+	eChunk := <-sock2.Read()
+	checkTypeAndSession(t, eChunk, testSession, ChunkType)
+
+	eChoke := <-sock2.Read()
+	checkTypeAndSession(t, eChoke, testSession, ChokeType)
+	<-onStop
 }
