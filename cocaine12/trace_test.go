ceeff8b29b90d365caf92e5f17edfbf4f418de94
Anton Tiurin <noxiouz@yandex.ru>
2015-09-26 11:19:30 +0300

    [Tests] Fix trace_test

--- cocaine12/trace_test.go
+++ cocaine12/trace_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func BenchmarkTraceWith(b *testing.B) {
-	ctx := BeginNewTraceContext()
+	ctx := BeginNewTraceContext(nil)
 	for n := 0; n < b.N; n++ {
 		_, _ = WithTrace(ctx, "bench")
 	}
