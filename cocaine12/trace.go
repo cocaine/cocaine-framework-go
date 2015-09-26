9de0c2f72d784bc1971928dbd1e2a3e4b343fc0d
Anton Tiurin <noxiouz@yandex.ru>
2015-09-26 11:50:23 +0300

    [Trace] Get rid of extra time.Now() calls. Add some docs, add a duration field

--- cocaine12/trace.go
+++ cocaine12/trace.go
@@ -31,6 +31,21 @@ func traceLog() Logger {
 	return traceLogger
 }
 
+func getTraceInfo(ctx context.Context) *TraceInfo {
+	if val, ok := ctx.Value(TraceInfoValue).(TraceInfo); ok {
+		return &val
+	}
+	return nil
+}
+
+// CloseSpan closes attached span. It should be call after
+// the rpc ends.
+type CloseSpan func(format string, args ...interface{})
+
+var (
+	closeDummySpan CloseSpan = func(format string, args ...interface{}) {}
+)
+
 type TraceInfo struct {
 	trace, span, parent uint64
 }
@@ -39,6 +54,17 @@ type traced struct {
 	context.Context
 	traceInfo TraceInfo
 	startTime time.Time
+}
+
+func (t *traced) Value(key interface{}) interface{} {
+	switch key {
+	case TraceInfoValue:
+		return t.traceInfo
+	case TraceStartTimeValue:
+		return t.startTime
+	default:
+		return t.Context.Value(key)
+	}
 }
 
 // It might be used in client applications.
@@ -71,45 +97,44 @@ func CleanTraceInfo(ctx context.Context) context.Context {
 	return context.WithValue(ctx, TraceInfoValue, nil)
 }
 
-func (t *traced) Value(key interface{}) interface{} {
-	switch key {
-	case TraceInfoValue:
-		return t.traceInfo
-	case TraceStartTimeValue:
-		return t.startTime
-	default:
-		return t.Context.Value(key)
-	}
-}
-
-func getTraceInfo(ctx context.Context) *TraceInfo {
-	if val, ok := ctx.Value(TraceInfoValue).(TraceInfo); ok {
-		return &val
-	}
-	return nil
-}
-
-func nullDone(format string, args ...interface{}) {}
-
+// WithTrace starts new span and returns a context with attached TraceInfo and Done.
+// If ctx is nil or has no TraceInfo new span won't start to support sampling,
+// so it's user responsibility to make sure that the context has TraceInfo.
+// Anyway it safe to call CloseSpan function even in this case, it actually does nothing.
 func WithTrace(ctx context.Context, rpcName string) (context.Context, func(format string, args ...interface{})) {
 	if ctx == nil {
-		return context.Background(), nullDone
+		// I'm not sure it is a valid action.
+		// According to the rule "no trace info, no new span"
+		// to support sampling, nil Context has no TraceInfo, so
+		// it cannot start new Span.
+		return context.Background(), closeDummySpan
 	}
 
 	traceInfo := getTraceInfo(ctx)
 	if traceInfo == nil {
-		return ctx, nullDone
+		// given context has no TraceInfo
+		// so we can't start new trace to support sampling.
+		// closeDummySpan does nohing
+		return ctx, closeDummySpan
 	}
 
+	// startTime is not used only to log the start of an RPC
+	// It's stored in Context to calculate the RPC call duration.
+	// A user can get it via Context.Value(TraceStartTimeValue)
 	startTime := time.Now()
 
+	// Tracing magic:
+	// * the previous span becomes our parent
+	// * new span is set as random number
+	// * trace still stays the same
 	traceInfo.parent = traceInfo.span
 	traceInfo.span = uint64(rand.Int63())
+
 	traceLog().WithFields(Fields{
 		"trace_id":  fmt.Sprintf("%x", traceInfo.trace),
 		"span_id":   fmt.Sprintf("%x", traceInfo.span),
 		"parent_id": fmt.Sprintf("%x", traceInfo.parent),
-		"timestamp": time.Now().UnixNano(),
+		"timestamp": startTime.UnixNano(),
 		"RPC":       rpcName,
 	}).Infof("start")
 
@@ -120,11 +145,14 @@ func WithTrace(ctx context.Context, rpcName string) (context.Context, func(forma
 	}
 
 	return ctx, func(format string, args ...interface{}) {
+		now := time.Now()
+		duration := now.Sub(startTime)
 		traceLog().WithFields(Fields{
 			"trace_id":  fmt.Sprintf("%x", traceInfo.trace),
 			"span_id":   fmt.Sprintf("%x", traceInfo.span),
 			"parent_id": fmt.Sprintf("%x", traceInfo.parent),
-			"timestamp": time.Now().UnixNano(),
+			"timestamp": now.UnixNano(),
+			"duration":  duration.Nanoseconds() / 1000,
 			"RPC":       rpcName,
 		}).Infof(format, args...)
 	}
