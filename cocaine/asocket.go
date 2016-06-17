6e5b837828a15753ef8581f53c6c872cc48b48f2
kompanets <kompanets@corp.sputnik.ru>
2016-06-17 18:48:03 +0300

    add log

--- cocaine/asocket.go
+++ cocaine/asocket.go
@@ -5,10 +5,10 @@ import (
 	"sync"
 	"time"
 
-	"log"
+	log "gitlab.srv.pv.km/common/logger"
 )
 
-var _ = log.Println
+//var _ = log.Println
 
 type socketIO interface {
 	Read() chan rawMessage
@@ -30,6 +30,7 @@ type asyncBuff struct {
 }
 
 func newAsyncBuf() *asyncBuff {
+	log.Info("Create new asyncBuff")
 	buf := asyncBuff{make(chan rawMessage), make(chan rawMessage), make(chan bool, 1), sync.WaitGroup{}}
 	buf.loop()
 	return &buf
@@ -37,6 +38,7 @@ func newAsyncBuf() *asyncBuff {
 
 func (bf *asyncBuff) loop() {
 	go func() {
+		log.Info("[asyncBuff] loop(): start")
 		bf.wg.Add(1)
 		defer bf.wg.Done()
 		var pending []rawMessage // data buffer
@@ -45,37 +47,49 @@ func (bf *asyncBuff) loop() {
 
 		finished := false // flag
 		for {
+			log.Info("[asyncBuff] loop(): next iteration")
 			var first rawMessage
 			var _out chan rawMessage
 			if len(pending) > 0 {
+				log.Info("[asyncBuff] loop(): changing the value of the first element")
 				first = pending[0]
 				_out = bf.out
 			} else if finished {
+				log.Info("[asyncBuff] loop(): finished")
 				close(bf.out)
+				log.Error("[asyncBuff] loop(): output channel is closed")
 				break
 			}
 			select {
 			case incoming, ok := <-_in:
 				if ok {
+					log.Info("[asyncBuff] loop(): got rawMessage")
 					pending = append(pending, incoming)
 				} else {
+					log.Error("[asyncBuff] loop(): input channel is closed")
 					finished = true
 					_in = nil
 				}
 			case _out <- first:
+				log.Infof("[asyncBuff] loop(): first element has been sent to channel")
 				pending[0] = nil
 				pending = pending[1:]
 			case <-bf.stop:
+				log.Infof("[asyncBuff] loop(): Notification from bf.stop")
 				close(bf.out) // Notify receiver
+				log.Info("[asyncBuff] loop(): end")
 				return
 			}
 		}
+		log.Info("[asyncBuff] loop(): end")
 	}()
 }
 
 func (bf *asyncBuff) Stop() (res bool) {
+	log.Info("[asyncBuff] Stop(): start")
 	close(bf.stop)
 	bf.wg.Wait()
+	log.Info("[asyncBuff] Stop(): end")
 	return
 }
 
@@ -106,9 +120,11 @@ func newAsyncRWSocket(family string, address string, timeout time.Duration) (*as
 }
 
 func (sock *asyncRWSocket) Close() {
+	log.Error("[asyncRWSocket] Close() start")
 	sock.clientToSock.Stop()
 	sock.socketToClient.Stop()
 	sock.close()
+	log.Error("[asyncRWSocket] Close() end")
 }
 
 func (sock *asyncRWSocket) close() {
@@ -136,9 +152,12 @@ func (sock *asyncRWSocket) Read() chan rawMessage {
 
 func (sock *asyncRWSocket) writeloop() {
 	go func() {
+		log.Infof("[asyncRWSocket] writeloop() start")
 		for incoming := range sock.clientToSock.out {
+			log.Infof("[asyncRWSocket] writeloop() write data")
 			_, err := sock.Conn.Write(incoming) //Add check for sending full
 			if err != nil {
+				log.Errorf("[asyncRWSocket] writeloop() error: %v", err)
 				sock.close()
 				// blackhole all writes
 				go func() {
@@ -149,27 +168,35 @@ func (sock *asyncRWSocket) writeloop() {
 				}()
 				return
 			}
+			log.Infof("[asyncRWSocket] writeloop() data has been written")
 		}
+		log.Infof("[asyncRWSocket] writeloop() end")
 	}()
 }
 
 func (sock *asyncRWSocket) readloop() {
 	go func() {
 		buf := make([]byte, 65536)
+		log.Infof("[asyncRWSocket] readloop() start")
 		for {
 			count, err := sock.Conn.Read(buf)
+			log.Infof("[asyncRWSocket] readloop() got data")
 			if count > 0 {
 				bufferToSend := make([]byte, count)
 				copy(bufferToSend[:], buf[:count])
+				log.Infof("[asyncRWSocket] readloop() send data to channel")
 				sock.socketToClient.in <- bufferToSend
+				log.Info("[asyncRWSocket] readloop(): data has been sent to channel")
 			}
 
 			if err != nil {
+				log.Errorf("[asyncRWSocket] readloop() error: %v", err)
 				close(sock.socketToClient.in)
 				sock.close()
 				return
 			}
 		}
+		log.Infof("[asyncRWSocket] readloop() end")
 	}()
 }
 
--- cocaine/service.go
+++ cocaine/service.go
@@ -3,6 +3,7 @@ package cocaine
 import (
 	"fmt"
 	"github.com/ugorji/go/codec"
+	log "gitlab.srv.pv.km/common/logger"
 	"sync"
 	"time"
 )
@@ -39,43 +40,43 @@ func (err *ServiceError) Error() string {
 	return err.Message
 }
 
-func getServiceChanPair(stop <-chan bool) (In chan ServiceResult, Out chan ServiceResult) {
-	In = make(chan ServiceResult)
-	Out = make(chan ServiceResult)
-	finished := false
-	go func() {
-		var pending []ServiceResult
-		for {
-			var out chan ServiceResult
-			var first ServiceResult
+//func getServiceChanPair(stop <-chan bool) (In chan ServiceResult, Out chan ServiceResult) {
+//	In = make(chan ServiceResult)
+//	Out = make(chan ServiceResult)
+//	finished := false
+//	go func() {
+//		var pending []ServiceResult
+//		for {
+//			var out chan ServiceResult
+//			var first ServiceResult
 
-			if len(pending) > 0 {
-				first = pending[0]
-				out = Out
-			} else if finished {
-				close(Out)
-				break
-			}
+//			if len(pending) > 0 {
+//				first = pending[0]
+//				out = Out
+//			} else if finished {
+//				close(Out)
+//				break
+//			}
 
-			select {
-			case incoming, ok := <-In:
-				if ok {
-					pending = append(pending, incoming)
-				} else {
-					finished = true
-					In = nil
-				}
+//			select {
+//			case incoming, ok := <-In:
+//				if ok {
+//					pending = append(pending, incoming)
+//				} else {
+//					finished = true
+//					In = nil
+//				}
 
-			case out <- first:
-				pending = pending[1:]
+//			case out <- first:
+//				pending = pending[1:]
 
-			case <-stop: // Notification from Close()
-				return
-			}
-		}
-	}()
-	return
-}
+//			case <-stop: // Notification from Close()
+//				return
+//			}
+//		}
+//	}()
+//	return
+//}
 
 //Allows you to invoke methods of services and send events to other cloud applications.
 type Service struct {
@@ -109,18 +110,22 @@ func serviceCreateIO(endpoint string) (sock socketIO, err error) {
 }
 
 func NewService(name string, args ...interface{}) (s *Service, err error) {
+	log.Infof("[%v] Create service", name)
 	info, err := serviceResolve(name, args...)
 	if err != nil {
+		log.Errorf("[%v] Resolve error: %v", name, err)
 		return
 	}
 
 	if !info.success {
 		err = fmt.Errorf("Unable to resolve service %s", name)
+		log.Errorf("[%v] %v", name, err)
 		return
 	}
 
 	sock, err := serviceCreateIO(info.Endpoint.AsString())
 	if err != nil {
+		log.Errorf("[%v] Create error: %v", name, err)
 		return
 	}
 
@@ -136,30 +141,45 @@ func NewService(name string, args ...interface{}) (s *Service, err error) {
 		wg:              sync.WaitGroup{},
 		is_reconnecting: false,
 	}
+	log.Infof("[%v] Service has been created", name)
 	go s.loop()
 	return
 }
 
 func (service *Service) loop() {
+	log.Infof("[%v] loop(): start", service.name)
+
 	for data := range service.socketIO.Read() {
 		for _, item := range service.unpacker.Feed(data) {
 			switch msg := item.(type) {
 			case *chunk:
+				log.Infof("[%v] loop(): got chunk", service.name)
 				if ch, ok := service.sessions.Get(msg.getSessionID()); ok {
+					log.Infof("[%v] loop(): send chunk to channel", service.name)
 					ch <- &serviceRes{msg.Data, nil}
+					log.Infof("[%v] loop(): chank has been sent to channel", service.name)
 				}
 			case *choke:
+				log.Infof("[%v] loop(): got choke", service.name)
 				if ch, ok := service.sessions.Get(msg.getSessionID()); ok {
+					log.Infof("[%v] loop(): close the channel", service.name)
 					close(ch)
 					service.sessions.Detach(msg.getSessionID())
+					log.Infof("[%v] loop(): Channel has been closed", service.name)
 				}
 			case *errorMsg:
+				log.Infof("[%v] loop(): got errorMsg", service.name)
 				if ch, ok := service.sessions.Get(msg.getSessionID()); ok {
+					log.Infof("[%v] loop(): send errorMsg to channel", service.name)
 					ch <- &serviceRes{nil, &ServiceError{msg.Code, msg.Message}}
+					log.Infof("[%v] loop(): errorMsg has been sent to channel", service.name)
 				}
+			default:
+				log.Errorf("[%v] loop(): got unknown message", service.name)
 			}
 		}
 	}
+	log.Infof("[%v] loop(): Send error to all open sessions", service.name)
 	for _, id := range service.sessions.Keys() {
 		if ch, ok := service.sessions.Get(id); ok {
 			ch <- &serviceRes{nil, &ServiceError{-1, "Disconnected"}}
@@ -167,13 +187,16 @@ func (service *Service) loop() {
 			service.sessions.Detach(id)
 		}
 	}
+	log.Infof("[%v] loop(): end", service.name)
 }
 
 func (service *Service) Reconnect(force bool) error {
+	log.Infof("[%v] Reconnect(): start", service.name)
 	if !service.is_reconnecting {
 		service.mutex.Lock()
 		defer service.mutex.Unlock()
 		if service.is_reconnecting {
+			log.Errorf("[%v] Reconnect(): Service is reconnecting now", service.name)
 			return fmt.Errorf("%s", "Service is reconnecting now")
 		}
 		service.is_reconnecting = true
@@ -183,10 +206,12 @@ func (service *Service) Reconnect(force bool) error {
 			select {
 			case <-service.IsClosed():
 			default:
+				log.Errorf("[%v] Reconnect(): Service is already connected", service.name)
 				return fmt.Errorf("%s", "Service is already connected")
 			}
 		}
 		// Send error to all open sessions
+		log.Infof("[%v] Reconnect(): Send error to all open sessions", service.name)
 		for _, key := range service.sessions.Keys() {
 			service.sessions.RLock()
 			fmt.Println(key)
@@ -198,29 +223,38 @@ func (service *Service) Reconnect(force bool) error {
 		}
 
 		// Create new socket
+		log.Infof("[%v] Reconnect(): Create new socket", service.name)
 		info, err := serviceResolve(service.name, service.args...)
 		if err != nil {
+			log.Errorf("[%v] Reconnect(): resolve error: %v", service.name, err)
 			return err
 		}
 		sock, err := serviceCreateIO(info.Endpoint.AsString())
 		if err != nil {
+			log.Errorf("[%v] Reconnect(): create error: %v", service.name, err)
 			return err
 		}
 
 		// Dispose old IO interface
+		log.Infof("[%v] Reconnect(): Dispose old IO interface", service.name)
 		service.Close()
 		service.stop = make(chan bool)
 		service.socketIO = sock
 		service.unpacker = newStreamUnpacker()
+
+		log.Infof("[%v] Reconnect(): run loop()", service.name)
 		go service.loop()
 		return nil
 	}
+	log.Errorf("[%v] Reconnect(): Service is reconnecting now", service.name)
 	return fmt.Errorf("%s", "Service is reconnecting now")
 }
 
 func (service *Service) call(name string, args ...interface{}) chan ServiceResult {
+	log.Infof("[%v] call(): start", service.name)
 	method, err := service.getMethodNumber(name)
 	if err != nil {
+		log.Infof("[%v] call(): error: %v", service.name, err)
 		errorOut := make(chan ServiceResult, 1)
 		errorOut <- &serviceRes{nil, &ServiceError{-100, "Wrong method name"}}
 		return errorOut
@@ -229,13 +263,16 @@ func (service *Service) call(name string, args ...interface{}) chan ServiceResul
 	id := service.sessions.Attach(in)
 	msg := ServiceMethod{messageInfo{method, id}, args}
 	service.socketIO.Write() <- packMsg(&msg)
+	log.Infof("[%v] call(): end", service.name)
 	return out
 }
 
 //Calls a remote method by name and pass args
 func (service *Service) Call(name string, args ...interface{}) chan ServiceResult {
+	log.Infof("[%v] Call(): start", service.name)
 	select {
 	case <-service.IsClosed():
+		log.Infof("[%v] Call(): start reconnect", service.name)
 		if err := service.Reconnect(false); err != nil {
 			errorOut := make(chan ServiceResult, 1)
 			errorOut <- &serviceRes{nil, &ServiceError{-32, "Disconnected"}}
@@ -243,51 +280,66 @@ func (service *Service) Call(name string, args ...interface{}) chan ServiceResul
 		}
 	default:
 	}
+	log.Infof("[%v] Call(): end", service.name)
 	return service.call(name, args...)
 }
 
 //Disposes resources of a service. You must call this method if the service isn't used anymore.
 func (service *Service) Close() {
+	log.Infof("[%v] Close(): start", service.name)
 	close(service.stop) // Broadcast all related goroutines about disposing
 	service.socketIO.Close()
+	log.Infof("[%v] Close(): end", service.name)
 }
 
 func (service *Service) getServiceChanPair() (In chan ServiceResult, Out chan ServiceResult) {
 	In = make(chan ServiceResult)
 	Out = make(chan ServiceResult)
 	go func() {
+		log.Infof("[%v] getServiceChanPair(): start", service.name)
 		service.wg.Add(1)
 		defer service.wg.Done()
 		finished := false
 		var pending []ServiceResult
 		for {
+			log.Infof("[%v] getServiceChanPair: next iteration", service.name)
 			var out chan ServiceResult
 			var first ServiceResult
 
 			if len(pending) > 0 {
+				log.Infof("[%v] getServiceChanPair: changing the value of the first element", service.name)
 				first = pending[0]
 				out = Out
 			} else if finished {
+				log.Infof("[%v] getServiceChanPair: finished", service.name)
 				close(Out)
+				log.Error("[asyncBuff] loop(): output channel is closed")
 				break
 			}
 
 			select {
 			case incoming, ok := <-In:
 				if ok {
+					log.Infof("[%v] getServiceChanPair: got ServiceResult", service.name)
 					pending = append(pending, incoming)
 				} else {
+					log.Errorf("[%v] getServiceChanPair: input channel is closed", service.name)
 					finished = true
 					In = nil
 				}
 
 			case out <- first:
+				log.Infof("[%v] getServiceChanPair: first element has been sent to channel", service.name)
 				pending = pending[1:]
 
 			case <-service.stop: // Notification from Close()
+				log.Infof("[%v] getServiceChanPair(): Notification from Close()", service.name)
+				close(Out)
+				log.Infof("[%v] getServiceChanPair(): end", service.name)
 				return
 			}
 		}
+		log.Infof("[%v] getServiceChanPair(): end", service.name)
 	}()
 	return
 }
